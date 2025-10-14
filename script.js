bdf44bda66e577d25ed0e25846fe66f3bba09213
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-10-14 23:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+
+function mapLetters(string) {
+	let obj = {};
+
+	for(let i=0; i<string.length; i++) {
+		if(!obj[string[i]]) {
+			obj[string[i]] = [];
+		}
+
+		obj[string[i]].push(i);
+	}
+
+	return obj;
+}
+mapLetters();
